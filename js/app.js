b71b5f276cddca3a091ea94ba516ab00b4e9df0a
tavstyle <tavstyle.gdl.5432@gmail.com>
2026-07-22 20:16:53 -0600

    Nuevo Hero y Barra lateral de Menú

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log("TavStyle Iniciado 🚀");
+
+const menuBtn = document.querySelector(".menu-btn");
+const closeMenu = document.querySelector(".close-menu");
+const sideMenu = document.querySelector(".side-menu");
+
+
+menuBtn.addEventListener("click", ()=>{
+
+    sideMenu.classList.add("active");
+
+});
+
+
+closeMenu.addEventListener("click", ()=>{
+
+    sideMenu.classList.remove("active");
+
+});
